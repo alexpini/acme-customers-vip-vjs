60a85ae93245fd6f442e07a71efe8851f5ca6d39
Alexandra Pini <alexandrampini@gmail.com>
2019-07-27 13:24:13 -0400

    commit

---
+++ scripts.js
@@ -0,0 +1,5 @@
+let customers = [
+  { id: 1, name: 'moe', email: 'moe@gmail.com', isVIP: true},
+  { id: 2, name: 'larry', isVIP: true, email: 'larry@gmail.com'},
+  { id: 4, name: 'shep', email: 'shep@gmail.com'},
+ ];
