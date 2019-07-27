f46683161d93ad9499dec1f6eafb303abcf35bb5
James Fuller <jfuller957@gmail.com>
2019-07-27 10:30:17 -0700

    game plan added

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
+/*
+Game Plan
+  build things out slowly
+  start with rendering the list
+  add delete functionality
+  add form
+  add ability to add a customer
+  add error handling
+  click to see functionality
+Winning
+*/
+
 let customers = [
   { id: 1, name: 'moe', email: 'moe@gmail.com', isVIP: true},
   { id: 2, name: 'larry', isVIP: true, email: 'larry@gmail.com'},
